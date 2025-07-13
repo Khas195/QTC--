00e92aa660c50f851ffae90295dc318c387815a6
Cao Thanh Tung <tungthanhcao1091995@gmail.com>
2025-07-13 13:38:35 +0200

    Getting Started
    Setup basic C++ main cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int a = 1;
+	int b = 2;
+	std::cout << "Cout:" << a << " + " << b << " = " << (a + b) << std::endl;
+	printf("Printf: %d + %d = %d \n", a, b, a + b);
+	return 0;
+}
